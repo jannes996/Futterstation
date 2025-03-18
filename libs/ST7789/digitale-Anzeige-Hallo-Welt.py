4a991b6ea6cc692b9a8992b4ad1073dd6241ca98
jannes996 <jannes.gonsio24@edu.bztg-oldenburg.de>
2025-03-18 10:34:31 +0100

    libs

---
+++ libs/ST7789/digitale-Anzeige-Hallo-Welt.py
@@ -0,0 +1,36 @@
+# Ersteller: Jannes Gonsior
+# Datum: 26.11.2024
+# Version 1.0
+# Programm zur Darstellung auf einem Display 
+
+# Import der Libarys
+from machine import Pin, SPI
+import st7789py as st7789
+import vga1_16x32 as font
+import time
+
+#scl=sck
+#sda=mosi
+
+# Variabeln und PIN's definieren
+spi = SPI(1,
+    baudrate=20000000,
+    polarity=0,
+    phase=0,
+    sck=Pin(18),
+    mosi=Pin(17),
+    miso=Pin(0))
+
+tft = st7789.ST7789(
+    spi,
+    240,
+    320,
+    reset=Pin(5, Pin.OUT),
+    cs=Pin(15, Pin.OUT),
+    dc=Pin(4, Pin.OUT),
+    backlight=Pin(0, Pin.OUT),
+    rotation=0)
+
+# Ausgabe
+tft.text(font, "Hallo Welt", 30, 30, st7789.WHITE, st7789.BLACK)
+
